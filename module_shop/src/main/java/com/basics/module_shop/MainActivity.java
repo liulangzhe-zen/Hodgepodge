dfa29f2250684971a7ed34bc33bdf178ec5c1a40
xueshijie <1014331570@qq.com>
2020-03-20 11:11:54 +0800

    1、划分模块与创建项目

---
+++ module_shop/src/main/java/com/basics/module_shop/MainActivity.java
@@ -0,0 +1,16 @@
+package com.basics.module_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.basics.module_shops.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
